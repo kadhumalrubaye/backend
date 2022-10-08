a6536071ac8cb675a74f0f40ba9d8bf08608fcb0
kadhumalrubaye <kadhum.it@gmail.com>
2022-10-08 19:41:27 +0300

    Update database config

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-18-209-78-11.compute-1.amazonaws.com'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd62in1ub8k2oga'),
-      user: env('DATABASE_USERNAME', 'ualsnyqswvctwl'),
-      password: env('DATABASE_PASSWORD', 'a5abaa29607240b1dcf19172093c55c51dfd633231c19683ec409553caf438e7'),
+      database: env('DATABASE_NAME', 'alsi'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'kadhum1992'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
